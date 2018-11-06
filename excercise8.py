5a6fdb23be384559e627fdaebfa191f9208a6ed7
VERA.AARON <2200839@fbisd.flourbluffschools.net>
2018-11-06 14:14:15 -0600

    wip

---
+++ excercise8.py
@@ -0,0 +1,10 @@
+def palindromic_odometer():
+    for i in range(1000000):
+        if (str(i).zfill(6))[2:] == (str(i).zfill(6))[5:1:-1]:
+            if (str(i+1).zfill(6)) == (str(i+1).zfill(6)[5:0:-1]:
+                if (str(i+3).zfill(6) == (str(i+3).zfill(6)[4:0:-1]:
+                    if (str(i+4).zfill(6) == (str(i+4).zfill(6)[5:0:-1]:
+                        pass
+                
+
+                                      
